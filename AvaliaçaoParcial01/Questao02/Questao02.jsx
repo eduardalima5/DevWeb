721138c87444303d6439c861189a36298ec0b047
Maria Eduarda Lima Mota <148507110+eduardalima5@users.noreply.github.com>
2024-08-21 09:59:53 -0300

    Add files via upload

---
+++ AvaliaçaoParcial01/Questao02/Questao02.jsx
@@ -0,0 +1,27 @@
+
+
+const questao02 = () => {
+
+
+    return (
+        <div>
+        <img    //imagem do pokemon de frente
+        src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+        id = "deFrente"
+        alt="Pokemon"
+        />
+        <img
+        scr = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/25.png"
+        id = "deCosta"
+        alt = "Pokemon"
+        />
+        <button
+        >
+            alternar  imagem
+        </button>
+        </div>
+    )
+
+}
+
+export default questao02
---
+++ AvaliaçaoParcial01/questao01/Questao01.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from "react"
+
+
+const Questao01A = ({lista, mostrarMaiorElemento}) => {
+    return (
+        <div></div>
+    )
+}
+function questao01B() {
+    const [maiores, setMaiores] = useState([])
+
+    const lista = [ 
+        {a: 10, b:3, c: 7},
+        {a: 5, b:-3, c: 9},
+        {a: 1, b:9, c: 40},
+    ]
+
+    const mostrarMaiorElemento = () => {
+        return lista.map(lista, index) = {
+        
+        }
+    }
+
+    return (
+        <div>
+            <h1>Maior elemento da lista</h1>
+        </div>
+    )
+}
+
+export default questao01A
